36a4818043e521696ab49c513b7ad5186cf1ef35
Zachary Sang <zacharysang@gmail.com>
2019-01-25 15:00:13 -0500

    Update test import paths
    * Each test now imports flock-mpi.js from the master component

--- project_service/test/tests/#20#iscatter-extra.test.js
+++ project_service/test/tests/#20#iscatter-extra.test.js
@@ -4,7 +4,7 @@ let parentPort = WorkerThreads.parentPort;
 let postMessage = (data) => {parentPort.postMessage(data)};
 
 // import the worker lib
-let mpi = require('../../static/flock-mpi.js');
+let mpi = require('../../../master/flock_server/static/flock-mpi.js');
 
 async function iscatterTestExtra() {
     const LEN = 110;
