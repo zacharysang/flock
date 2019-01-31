53b121f3b782b87d15df6a74a46579117a973a4e
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-01-31 11:26:41 -0500

    Add auth blueprint

--- master/flock_server/__init__.py
+++ master/flock_server/__init__.py
@@ -6,16 +6,17 @@ from flask import (
     render_template
 )
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
-      #load the instince config if it exists when not testting
-      app.config.from_pyfile('config.py', silent=True)
+        # load the instince config if it exists when not testting
+        app.config.from_pyfile('config.py', silent=True)
     else:
-      # load the test config if passed in
-      app.config.from_mapping(test_config)
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
     
     # ensure the instance folder exists
     try:
@@ -28,7 +29,14 @@ def create_app(test_config=None):
     def get_index():
         return render_template('index.html')
 
+    #
     # load blueprints
+    #
+    # auth
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    # work module
     from . import work
     app.register_blueprint(work.bp)
 
