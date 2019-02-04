2a2dd3b08c4b976a4fda9f13244fdd3be879a00a
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-02-04 18:23:49 -0500

    Write html for login/register page

--- master/flock_server/auth.py
+++ master/flock_server/auth.py
@@ -6,7 +6,7 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint('auth', __name__)
 
 
 @bp.route('/login', methods=('GET', 'POST'))
