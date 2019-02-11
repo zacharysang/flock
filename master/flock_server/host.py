c1a50bd2a04fa1c49c791c8bb9fa3c7233e01c2f
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-02-11 18:30:41 -0500

    Add backend for submitting projects for review

--- master/flock_server/host.py
+++ master/flock_server/host.py
@@ -5,6 +5,7 @@ from flask import (
 )
 
 from flock_server.db import get_db
+from flock_server.auth import auth_required, super_user_permissions_required
 
 bp = Blueprint('host', __name__)
 
@@ -16,9 +17,40 @@ def queue():
     return render_template('host/queue.html')
 
 
-@bp.route('/submit')
+@bp.route('/submit', methods=('GET', 'POST'))
+@auth_required
 def submit_project():
     """For submitting of new projects.
     """
+    if request.method == 'POST':
+        # pull the values from the request
+        name = request.form['name']
+        source_url = request.form['source-url']
+        description = request.form['description']
+
+        # setup the database
+        db = get_db()
+        cursor = db.cursor()
+        error = None
+        
+        # check that user input exists
+        if not name:
+            error = 'Name is required.'
+        elif not source_url:
+            error = 'Source URL is required.'
+        # description is not required
+
+        if error is None:
+            # good to go forward with input
+            cursor.execute(
+                ('INSERT INTO projects (name, source_url, description, '
+                'owner_id) VALUES (?, ?, ?, ?)'),
+                (name, source_url, description, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+        # there was an error, fall through with flash
+        flash(error)
 
     return render_template('host/submit.html')
--- master/flock_server/schema.sql
+++ master/flock_server/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   source_url TEXT NOT NULL,
-  deployment_url TEXT NOT NULL,
+  deployment_url TEXT,
   approval_status INTEGER NOT NULL DEFAULT 0,
   description TEXT,
   owner_id INTEGER NOT NULL,
