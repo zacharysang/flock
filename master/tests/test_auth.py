238f1f85b7e42bc7294e0344db4f85403303f785
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-02-19 13:26:46 -0500

    Add authentication tests

--- master/tests/test_auth.py
+++ master/tests/test_auth.py
@@ -23,3 +23,49 @@ def test_register(client, app):
             'SELECT * FROM users WHERE email = (?)',
             ('test-reg@email.com',)
         ).fetchone() is not None
+
+@pytest.mark.parametrize(('email', 'password', 'confirm_password', 'message'), (
+    ('', 'pass', 'pass',  b'Email is required.'),
+    ('email', '', 'pass', b'Password is required.'),
+    ('email', 'pass', '', b'Password confirmation is required.'),
+    ('email', 'pass', 'not-pass', b'Passwords do not match.'),
+    # test@email.com is in data.sql
+    ('test@email.com', 'pass', 'pass', b'Account cannot be registered'),
+))
+def test_register_validate_input(client, email, password, confirm_password,
+                                 message):
+    response = client.post(
+        '/register',
+        data={'email': email, 'password': password,
+              'confirm-password': confirm_password
+        }
+    )
+    assert message in response.data
+
+def test_login(client, auth):
+    assert client.get('/login').status_code == 200
+    response = auth.login()
+    assert response.headers['Location'].endswith('/')
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['email'] == 'test@email.com'
+
+@pytest.mark.parametrize(('email', 'password', 'message'), (
+    ('', 'test', b'Email is required.'),
+    ('a', '', b'Password is required.'),
+    ('a', 'test', b'Incorrect email.'),
+    ('test@email.com', 'abc', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, email, password, message):
+    response = auth.login(email, password)
+    assert message in response.data
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert 'user_id' not in session
+    
