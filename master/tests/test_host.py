5edf6b076a147f9ea725c9c187b02978ee7f867c
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-02-26 23:10:52 -0500

    Add more  host tests

--- master/tests/test_host.py
+++ master/tests/test_host.py
@@ -11,6 +11,17 @@ def test_login_required(client, path):
     assert response.headers['Location'].endswith('/login')
 
 @pytest.mark.parametrize('path', (
+    '/host/queue',
+    '/host/1/approve'
+))
+def test_super_login_required(client, auth, path):
+    auth.login()
+    # should redirect to home page
+    response = client.get(path, follow_redirects=True)
+    # will be message stating that the user can't access this page
+    assert b'super user' in response.data
+
+@pytest.mark.parametrize('path', (
     '/host/1',
 ))
 def test_owner_required(client, auth, path):
@@ -19,4 +30,18 @@ def test_owner_required(client, auth, path):
     response = client.get(path, follow_redirects=True)
     assert b'permissions' in response.data
 
+@pytest.mark.parametrize(('name', 'source_url', 'message'), (
+    ('', 'https://kurtjlewis.com', b'Name is required.'),
+    ('test proj', '', b'Source URL is required')
+))
+def test_submit_project(client, auth, name, source_url, message):
+    auth.login()
+
+    response = client.post(
+        '/host/submit',
+        data= { 'name' : name, 'source-url': source_url,
+                'description' : '' }
+    )
+    print(response.data)
+    assert message in response.data
 
