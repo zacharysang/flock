2cd8f31c2f000c81247ca5ce3913b4d4bbaa1fa9
Laura Tebben <tebbenla@mail.uc.edu>
2019-03-26 19:03:18 -0400

    test

--- project_service/test/flock-tests/dki.js
+++ project_service/test/flock-tests/dki.js
@@ -169,6 +169,7 @@ async function main() {
         console.log('root sending and receiving links');
         for (var idx = 0; idx < size; idx++) {
             receiveMessages.push(mpi.irecv(idx + 1, 'default'));
+            console.log(receiveMessages)
             if (sources.length > 0) {
                 mpi.isend(sources.pop(), idx + 1, 'default');
                 outstandingReqs++;
