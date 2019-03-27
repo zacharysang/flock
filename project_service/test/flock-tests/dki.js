179971e2c672b5a785f3678d9def31fb3384c175
Laura Tebben <tebbenla@mail.uc.edu>
2019-03-26 22:09:17 -0400

    time 10k links

--- project_service/test/flock-tests/dki.js
+++ project_service/test/flock-tests/dki.js
@@ -204,7 +204,7 @@ async function main() {
                     for (var jdx = 0; jdx < links.length; jdx++) {
                         link = links[jdx];
                         console.log('evaluating link: '+link);
-                        console.log('length of explored: '+explored.length);
+                        console.log('length of explored: '+explored.size);
                         if (!explored.has(link)) {
                             sources.push(link);
                             explored.add(link);
