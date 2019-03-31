e79474a49b285c08990b48ce2b25fa42da4525f6
Laura Tebben <tebbenla@mail.uc.edu>
2019-03-31 18:32:19 -0400

    blob link sending to lower network load

--- project_service/test/flock-tests/dki.js
+++ project_service/test/flock-tests/dki.js
@@ -25,30 +25,30 @@ class Scrape {
 
     findLinks(html) {
         //console.log(html);
-        var all_links = [];
-        var ret_links = [];
+        let all_links = [];
+        let ret_links = [];
 
-        var re = /<a href\s*=\s*"(((?!\s).)*)"/g
+        let re = /<a href\s*=\s*"(((?!\s).)*)"/g
         do {
-            var m = re.exec(html);
+            let m = re.exec(html);
             if (m) {
                 all_links.push(m[1]);
             }
         } while (m);
 
-        // var doc = document.createElement("html");
+        // let doc = document.createElement("html");
         // doc.innerHTML = html;
-        // var links = doc.getElementsByTagName("a")
+        // let links = doc.getElementsByTagName("a")
 
-        // for (var i=0; i<links.length; i++) {
+        // for (let i=0; i<links.length; i++) {
         //     all_links.push(links[i].getAttribute("href"));
         // }
 
         for (var idx = 0; idx < all_links.length; idx++) {
-            var l = all_links[idx];
+            let l = all_links[idx];
             if (l != null && !l.includes('video.') && !l.includes('/video/') && !l.includes('/video?')) {
-                var regex = /https?:\/\/(www\.)?(.*(\.com|\.org))(\/)?\.*/gi;
-                var link = regex.exec(l);
+                let regex = /https?:\/\/(www\.)?(.*(\.com|\.org))(\/)?\.*/gi;
+                let link = regex.exec(l);
                 //console.log(link, this.url);
                 if (link && link.length >= 3 && (link[2].includes(this.url) || this.url.includes(link[2]))) {
                     ret_links.push(l);
@@ -60,7 +60,7 @@ class Scrape {
         //mpi.storeSet('ret_links', ret_links);
 
         this.num_discovered_links += ret_links.length;
-        var tmp = this.num_discovered_links;
+        let tmp = this.num_discovered_links;
         mpi.updateStatus({ 'numParsedLinks': tmp });
         //console.log('retlinks: ' + ret_links)
         return ret_links;
@@ -68,8 +68,8 @@ class Scrape {
 
     async makeRequest(url) {
         try {
-            var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
-            const res = await fetch(cors_api_url+url);
+            let cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+            const res = await fetch(cors_api_url + url);
             const response = await res.text();
             return response;
         }
@@ -79,8 +79,8 @@ class Scrape {
     }
 
     keywordClean(word) {
-        var word = word.replace(/\W/g, '');
-        var word = word.replace(/\d/g, '');
+        let word = word.replace(/\W/g, '');
+        let word = word.replace(/\d/g, '');
         if (word == '') {
             return null;
         }
@@ -88,11 +88,11 @@ class Scrape {
     }
 
     /*findKeywords(html) {
-        var ps = [];
-        var endps = [];
+        let ps = [];
+        let endps = [];
 
-        var re = /<p>/g;
-        var re2 = /<\/p>/g;
+        let re = /<p>/g;
+        let re2 = /<\/p>/g;
 
         do {
             m = re.exec(html);
@@ -116,7 +116,7 @@ class Scrape {
     }*/
 
     async scrape() {
-        var r = null;
+        let r = null;
         try {
             r = await this.makeRequest(this.url);
         } catch {
@@ -128,7 +128,7 @@ class Scrape {
             return [null, null];
         }
         //console.log('value of r after makeRequest: '+r);
-        var links = this.findLinks(r);
+        let links = this.findLinks(r);
         return [null, links];
     }
 }
@@ -150,7 +150,7 @@ async function main() {
 
     let sources = ['https://bbc.com', 'https://cincinnati.com', 'https://foxnews.com', 'https://npr.org/sections/news/', 'https://nytimes.com', 'https://forbes.com', 'https://wsj.com', 'https://www.cnn.com/', 'https://www.nbcnews.com/', 'https://abcnews.go.com/', 'https://www.yahoo.com/news/', 'https://www.washingtonpost.com/', 'https://www.theguardian.com/us', 'https://www.latimes.com/', 'https://www.apnews.com/', 'https://www.economist.com/', 'https://www.ap.org/en-us/', 'https://www.reuters.com/', 'https://www.bloomberg.com/', 'https://www.foreignaffairs.com/', 'https://www.theatlantic.com/', 'https://www.politico.com/', 'https://time.com/', 'https://www.cbsnews.com/'];
     //politico, cnn
-    //var sources = ['https://www.cnn.com/', 'https://www.politico.com/']
+    //let sources = ['https://www.cnn.com/', 'https://www.politico.com/']
     let outstandingReqs = 0;
     let receiveMessages = [];
     let explored = new Set();
@@ -165,13 +165,16 @@ async function main() {
     if (rank == 0) {
         console.log('root sending and receiving links');
         for (var idx = 0; idx < size - 1; idx++) {
-            receiveMessages.push([idx+1,mpi.irecv(idx + 1, 'default')]);
+            receiveMessages.push([idx + 1, mpi.irecv(idx + 1, 'default')]);
             console.log('received from worker: ' + receiveMessages);
             if (sources.length > 0) {
-                mpi.isend(sources.pop(), idx + 1, 'default');
+                let blobsize = Math.floor(sources.length / (size - 1));
+                let blob = sources.slice(0, blobsize);
+                mpi.isend(blob, idx + 1, 'default');
+                sources = sources.slice(blobsize, sources.length - 1)
                 outstandingReqs++;
             } else {
-                mpi.isend('', idx + 1, 'default');
+                mpi.isend([''], idx + 1, 'default');
             }
         }
         starttime = Date.now();
@@ -179,31 +182,34 @@ async function main() {
         console.log('root sending and receiving more links');
         while (outstandingReqs > 0 && (stopTime < 0 || Date() < stopTime)) {
             for (var idx = 0; idx < receiveMessages.length; idx++) {
-                var res = await receiveMessages[idx][1];
-                //var res = req[1];
-                var rec_rank = receiveMessages[idx][0];
-                console.log('rank 0 recieved from child: '+receiveMessages[idx])
+                let res = await receiveMessages[idx][1];
+                //let res = req[1];
+                let rec_rank = receiveMessages[idx][0];
+                console.log('rank 0 recieved from child: ' + receiveMessages[idx])
                 if (res) {
                     outstandingReqs--;
                     //keywords = res[0];
                     links = res;
 
                     total += links.length;
-                    if (total >= 5000 && !flag){
-                        flag=true;
-                        var t = Date.now()-starttime;
-                        mpi.updateStatus({'timeto5k': t});
-                        console.log({'timeto5k': t});
+                    if (total >= 5000 && !flag) {
+                        flag = true;
+                        let t = Date.now() - starttime;
+                        mpi.updateStatus({ 'timeto5k': t });
+                        console.log({ 'timeto5k': t });
                     }
 
                     if (sources.length > 0) {
                         //console.log('sources: '+sources);
-                        nextLink = sources.pop();
-                        console.log('sending to child: '+nextLink);
-                        mpi.isend(nextLink, rec_rank, 'default');
+                        let blobsize = Math.floor(sources.length / (size - 1));
+                        let blob = sources.slice(0, blobsize);
+                        console.log('sending to child: ' + blob);
+                        mpi.isend(blob, rec_rank, 'default');
+                        sources = sources.slice(blobsize, sources.length - 1)
+
                         outstandingReqs++;
                     } else {
-                        mpi.isend('', rec_rank, 'default');
+                        mpi.isend([''], rec_rank, 'default');
                     }
 
                     receiveMessages.push(mpi.irecv(rec_rank, 'default'));
@@ -214,13 +220,13 @@ async function main() {
 
                     for (var jdx = 0; jdx < links.length; jdx++) {
                         link = links[jdx];
-                        
-                        console.log('evaluating link: '+link);
-                        console.log('length of explored: '+explored.size);
+
+                        console.log('evaluating link: ' + link);
+                        console.log('length of explored: ' + explored.size);
                         if (!explored.has(link)) {
                             sources.push(link);
                             explored.add(link);
-                            mpi.updateStatus({'numExploredLinks':explored.length});
+                            mpi.updateStatus({ 'numExploredLinks': explored.length });
                             // if (explored.size >= 5000){
                             //     mpi.updateStatus({'timeto5kUNIQUE': Date.now()-starttime});
                             //     return;
@@ -237,26 +243,29 @@ async function main() {
     } else {
         console.log('in worker node');
 
-        var it = 0;
+        let it = 0;
         while (stopTime < 0 || Date() < stopTime) {
             it++;
-            source = await mpi.irecv(0, 'default');
-            var links = [];
-            var keywords = '';
+            let source_arr = await mpi.irecv(0, 'default');
+            let links = [];
+            let keywords = '';
 
-            if (source == '') {
+            if (source_arr == ['']) {
                 await sleep(1);
             } else {
-                console.log('received link from root ' + source);
-                source = source.trim();
-                parts = source.split('/');
-                baseurl = parts.join('/');
-                s.setUrl(source);
-                retval = await s.scrape();
-                //console.log('result of scrape: ' + retval)
-                keywords = retval[0];
-                links = retval[1];
-                //console.log('links discovered: ' + links);
+                for (var idx = 0; idx < source_arr.length; idx++) {
+                    let source = source_arr[idx];
+                    console.log('received link from root ' + source);
+                    source = source.trim();
+                    parts = source.split('/');
+                    baseurl = parts.join('/');
+                    s.setUrl(source);
+                    retval = await s.scrape();
+                    //console.log('result of scrape: ' + retval)
+                    keywords.push(retval[0]);
+                    links.push(retval[1]);
+                    //console.log('links discovered: ' + links);
+                }
             }
             len = ((links) ? links.length : 0);
             console.log('sending discovered links to root ' + len.toString());
@@ -266,5 +275,5 @@ async function main() {
     }
 }
 
-var starttime = 0;
+let starttime = 0;
 main();
