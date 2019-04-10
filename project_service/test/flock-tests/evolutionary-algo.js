4688ce3fd4ff787a9da060592edc436286e933f7
Zach Collins <collinzy@mail.uc.edu>
2019-04-10 13:53:52 -0400

    finish evolutionary algorithm and display

--- project_service/test/flock-tests/evolutionary-algo.js
+++ project_service/test/flock-tests/evolutionary-algo.js
@@ -3,7 +3,17 @@
 // import flock-mpi
 importScripts('/static/flock-mpi.js');
 
-function fitness_eval(individual) {
+/**
+ * Measures the fitness of an individual. 0 = most fit.
+ * Maximally fit individual is alternating string of 0101...
+ * Worst possible fitness is length of the genome.
+ * Stateless fitness eval. Pass in an individual, have its fitness returned.
+ * 
+ * @param {object} individual an individual consisting of a fitness and a genome
+ * 
+ * @returns {number} the fitness of the individual
+ */
+function fitnessEval(individual) {
     let fitness = 0;
     for (let i = 0; i < individual.genome.length; i++) {
         if (i % 2 === 0 && individual.genome[i] === 1) {
@@ -16,12 +26,30 @@ function fitness_eval(individual) {
     return fitness;
 }
 
+/**
+ * Get a random integer from range [min, max] inclusive.
+ * 
+ * @param {number} min minimum for returned integer
+ * @param {number} max maximum for returned integer
+ * 
+ * @returns {number} a random integer
+ */
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Tournament-style selection of one individual from a population.
+ * Selects tournSize individuals, and returns the best among them.
+ * Assumes fitnesses are already evaluated.
+ * 
+ * @param {array} population a population of individuals to select from
+ * @param {number} tournSize number of individuals in the tournament
+ * 
+ * @returns {object} the best individual selected in the tournament
+ */
 function tournamentSelect(population, tournSize) {
     let max = population.length - 1;
     let participants = [];
@@ -31,6 +59,14 @@ function tournamentSelect(population, tournSize) {
     return bestIndInPopulation(participants);
 }
 
+/**
+ * Gets the index of the best individual in a population.
+ * Assumes fitnesses are already evaluated.
+ * 
+ * @param {array} population The population to search in
+ * 
+ * @returns {number} the index of the best individual in the population array
+ */
 function idxOfBestIndInPopulation(population) {
     let best_idx = 0;
     let best_fit = Number.MAX_SAFE_INTEGER;
@@ -43,14 +79,40 @@ function idxOfBestIndInPopulation(population) {
     return best_idx;
 }
 
+/**
+ * Gets the best individual in a population.
+ * Assumes fitnesses are already evaluated.
+ * 
+ * @param {array} population The population to search in
+ * 
+ * @returns {object} The best individual object in the population array
+ */
 function bestIndInPopulation(population) {
     return population[idxOfBestIndInPopulation(population)];
 }
 
+/**
+ * Gets the best fitness in the population.
+ * Assumes fitnesses are already evaluated.
+ * 
+ * @param {array} population The population to search in
+ * 
+ * @returns {number} The maximum fitness in the population
+ */
 function bestFitnessInPopulation(population) {
     return population[idxOfBestIndInPopulation(population)].fitness;
 }
 
+/**
+ * Random mutation function. Sateless - returns a copy of the individual with mutations applied.
+ * Does not modify individual passed in.
+ * Selects 0 to 10 bits in the genome to mutate, randomly assigns 0 or 1 to each of those bits.
+ * You must manually reevaluate fitness after mutating.
+ * 
+ * @param {object} individual The individual to mutate
+ * 
+ * @returns {object} A new individual - mutated version of the entered individual.
+ */
 function mutate(individual) {
     let m_genome = individual.genome.slice()
     let num_mutations = getRandomInt(0, 10);
@@ -61,6 +123,13 @@ function mutate(individual) {
     return {fitness: Number.MAX_SAFE_INTEGER, genome: m_genome};
 }
 
+/**
+ * Produces a shallow copy of an individual.
+ * 
+ * @param {object} individual the individual to copy
+ * 
+ * @returns {object} A shallow copy of the individual
+ */
 function clone(individual) {
     n_fitness = individual.fitness;
     n_genome = individual.genome.slice()
@@ -68,30 +137,56 @@ function clone(individual) {
     return new_ind;
 }
 
-function measure_fitnesses(population) {
+/**
+ * Stateful fitness measurement of a population.
+ * Modifies the fitness of each individual in the population passed in.
+ * 
+ * @param {array} population The population whose fitness should be measured
+ */
+function measureFitness(population) {
     for (let i = 0; i < population.length; i++) {
-        population[i].fitness = fitness_eval(population[i]);
+        population[i].fitness = fitnessEval(population[i]);
     }
 }
 
-async function distributed_measure_fitnesses(population) {
+/**
+ * A distributed way to measure fitness. Scatters individuals among all nodes.
+ * Very effective if fitness evaluation takes a long time.
+ * Does not modify the original population - the returned item is a shallow copy.
+ * 
+ * @param {array} population The population whose fitness should be measured
+ * 
+ * @returns {array} The population with fitnesses evaluated
+ */
+async function distributedMeasureFitness(population) {
     let res = await mpi.iscatter(population, 0, 'default');
 
-    measure_fitnesses(res);
+    measureFitness(res);
     await sleep(1000);
     statistics = stats(res);
-    mpi.updateStatus({
-        "Local Fitness Statistics\n(smaller is better)": "Best: " + statistics[0] +
-            ", Worst: " + statistics[1] + ", Average: " + statistics[2]
-    });
 
     return await mpi.igather(res, 0, 'default');
 }
 
+/**
+ * Sleep utility function. Await this function to actually pause.
+ * 
+ * @param {number} ms number of milliseconds to sleep
+ * 
+ * @returns {promise} a promise that sleeps ms seconds
+ */
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * Measure the best, worst, and average fitness in a population.
+ * Assumes fitnesses are already evaluated.
+ * 
+ * @param {array} population The population whose statistics should be measured
+ * 
+ * @returns {array} [best, worst, average]
+ */
 function stats(population) {
     let best = Number.MAX_SAFE_INTEGER;
     let worst = 0;
@@ -107,6 +202,29 @@ function stats(population) {
     });
     avg = avg / population.length;
     return [best, worst, avg];
+}
+
+/**
+ * Calculate the progress as a percentage, based on current best fitness and worst possible fitness.
+ * 
+ * @param {number} fitness The fitness whose progress to measure
+ * @param {number} genomeLength The length of the genome the fitness is measuring
+ * 
+ * @returns {number} The progress of the evolutionary algorithm
+ */
+function calculateProgress(fitness, genomeLength) {
+    return Math.floor(100 * (genomeLength - fitness) / genomeLength);
+}
+
+async function broadcastDisplay(population, gen, genomeLength) {
+    console.log("Statistics after generation " + gen + "...");
+    statistics = stats(population);
+    console.log("Best:    " + statistics[0]);
+    console.log("Worst:   " + statistics[1]);
+    console.log("Average: " + statistics[2]);
+    progress = calculateProgress(statistics[0], genomeLength);
+    best_ind = bestIndInPopulation(population);
+    await mpi.ibcast({statistics: statistics, gen: gen, progress: progress, best: best_ind}, 0, 'default');
 }
 
 async function main() {
@@ -125,32 +243,26 @@ async function main() {
     console.log(`got size: ${size}`);
     
     if (rank === 0) {
+
         while (true) {
             let population = [];
             let popSize = 1000;
             let genomeLength = 128;
+            let n_gen = 0;
             
             let MUTPB = 0.8;
 
             for (let i = 0; i < popSize; i++) {
-                let gen = Array.from({length: genomeLength}, () => getRandomInt(0, 1));
+                let genome = Array.from({length: genomeLength}, () => getRandomInt(0, 1));
                 population.push({
                     fitness: Number.MAX_SAFE_INTEGER,
-                    genome: gen
+                    genome: genome
                 });
             }
             
-            population = await distributed_measure_fitnesses(population);
-            console.log("Population: ");
-            console.log(population);
+            population = await distributedMeasureFitness(population);
+            await broadcastDisplay(population, n_gen, genomeLength);
 
-            console.log("Initial population statistics...");
-            let statistics = stats(population);
-            console.log("Best:    " + statistics[0]);
-            console.log("Worst:   " + statistics[1]);
-            console.log("Average: " + statistics[2]);
-
-            let n_gen = 0;
             while (bestFitnessInPopulation(population) > 0) {
                 let next_population = [];
 
@@ -162,15 +274,10 @@ async function main() {
                     next_population.push(new_ind);
                 }
 
-                population = await distributed_measure_fitnesses(next_population);
+                population = await distributedMeasureFitness(next_population);
+                await broadcastDisplay(population, n_gen, genomeLength);
 
                 n_gen++;
-
-                console.log("Statistics after generation " + n_gen + "...");
-                statistics = stats(population);
-                console.log("Best:    " + statistics[0]);
-                console.log("Worst:   " + statistics[1]);
-                console.log("Average: " + statistics[2]);
             }
 
             console.log("Finished!");
@@ -181,7 +288,16 @@ async function main() {
     } else {
 
         while (true) {
-            await distributed_measure_fitnesses([]);
+            await distributedMeasureFitness([]);
+            console.log("Measured fitness.");
+            let disp = await mpi.ibcast(0, 0, 'default');
+            console.log("Got broadcast.");
+            mpi.updateStatus({
+                "Fitness Statistics\n(smaller is better)": "Generations: " + disp.gen + "\nBest: " + disp.statistics[0] +
+                    "\nWorst: " + disp.statistics[1] + "\nAverage: " + disp.statistics[2],
+                progress: {reset: true, increment: disp.progress},
+                "Best Individual:": disp.best.genome.join("")
+            });
         }
 
     }
