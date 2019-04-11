471457496f4dbbb7ce22051691213566c4889483
Laura Tebben <tebbenla@mail.uc.edu>
2019-04-11 10:57:27 -0400

    remove remote debug flad from dev

--- project_service/app.js
+++ project_service/app.js
@@ -414,7 +414,6 @@ let idsByRank = {[MPI_COMM_WORLD]: {}};
     
     // Bind webServer to listening on PORT
 
-    console.log(PORT);
     webServer.listen(PORT, function () { console.log(`listening on http://localhost:${PORT}`); });
     
     let flock_url;
@@ -436,7 +435,11 @@ async function initializeNode0(url) {
     // Launch headless chrome
     // TODO Create a dedicated user to run headless chrome so that sandbox args can be removed and security improved (see here: https://github.com/GoogleChromeLabs/lighthousebot/blob/master/builder/Dockerfile#L35-L40)
 
-    let browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox','--remote-debugging-port=9222'], dumpio: RANK_0_DEBUG});
+    let args_arr = ['--no-sandbox', '--disable-setuid-sandbox'];
+    if (IS_DEV) {
+        args_arr.push('--remote-debugging-port=9222');
+    }
+    let browser = await puppeteer.launch({args: args_arr, dumpio: RANK_0_DEBUG});
 
     let page = await browser.newPage();
     await page.goto(url);
