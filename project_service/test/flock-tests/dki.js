56b3cee6427666525f2296e4dd0254d0c34952da
Laura Tebben <tebbenla@mail.uc.edu>
2019-04-14 18:23:14 -0400

    Stand up own proxy

--- project_service/test/flock-tests/dki.js
+++ project_service/test/flock-tests/dki.js
@@ -14,14 +14,15 @@ class Scrape {
     }
 
     setUrl(url) {
+        // URL to scrape
         this.url = url;
     }
 
     findLinks(html) {
-        //console.log(html);
         let all_links = [];
         let ret_links = [];
 
+        // Grab the all the links from the provided HTML
         let re = /<a href\s*=\s*"(((?!\s).)*)"/g;
         do {
             var m = re.exec(html);
@@ -30,38 +31,46 @@ class Scrape {
             }
         } while (m);
 
+        // Parse these links and remove the ones we're not interested in --
+        // No videos, no external links
         for (let idx = 0; idx < all_links.length; idx++) {
             let l = all_links[idx];
             if (l != null && !l.includes('video.') && !l.includes('/video/') && !l.includes('/video?')) {
                 let regex = /https?:\/\/(www\.)?(.*(\.com|\.org))(\/)?\.*/gi;
                 let link = regex.exec(l);
-                //console.log(link, this.url);
+
+                // The link must be to the same site as we're currently on
                 if (link && link.length >= 3 && (link[2].includes(this.url) || this.url.includes(link[2]))) {
                     ret_links.push(l);
-                } else if (l.startsWith('/') && l.length > 1) {
+                }
+                else if (l.startsWith('/') && l.length > 1) {
                     ret_links.push(this.url + l)
                 }
             }
         }
-        //mpi.storeSet('ret_links', ret_links);
 
+        // Update the UI with the number of discovered links
         this.num_discovered_links += ret_links.length;
+        // For some reason JS did not like this.num_discovered_links in the call to updateStatus so had to use
+        // intermediate variable
         let tmp = this.num_discovered_links;
         mpi.updateStatus({'Total number of links discovered by this node': tmp});
+
+        // Return the list of links
         return ret_links;
     }
 
     async makeRequest(url) {
         try {
-            let cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+            // Because we're making requests from a webpage, we need CORS headers at the destinations
+            // But nobody has those, so instead we have to send all our requests through this proxy I am hosting
+            let cors_api_url = 'https://ec2-18-217-115-53.us-east-2.compute.amazonaws.com:8080/';
             const res = await fetch(cors_api_url + url);
+
+            // Get the response, check status code, and return the page's text
             const response = await res;
             if (response.status !== 200) {
                 console.log(response.status, 'not 200');
-                /*if (response.status === 429) {
-                    console.log(response.status, 'waiting 5 seconds');
-                    await sleep(5);
-                }*/
             }
             return response.text();
         } catch (error) {
@@ -70,6 +79,7 @@ class Scrape {
     }
 
     keywordClean(word) {
+        // Remove nonwords
         word = word.replace(/\W/g, '');
         word = word.replace(/\d/g, '');
         if (word == '') {
@@ -82,7 +92,7 @@ class Scrape {
         let keywords = new Set();
         let ps = [];
 
-
+        // Recursively parse p tags
         function rec(html) {
             let re = /<p.*>(.*)<\/p>/g;
 
@@ -101,15 +111,19 @@ class Scrape {
 
         rec(html);
 
+        // Read each paragraph and extract the keywords
         for (let i = 0; i < ps.length; i++) {
             let paragraph = ps[i];
             paragraph = paragraph.toLowerCase();
 
+            // Remove non-words
             let words = paragraph.split(/\s/);
             words = words.map(this.keywordClean);
 
+            // Don't bother if the paragraph is short
             if (words.length < 10) continue;
 
+            // Remove stopwords
             for (let idx = 0; idx < words.length; idx++) {
                 let word = words[idx];
                 if (word && !this.stopWords.has(word)) {
@@ -117,6 +131,8 @@ class Scrape {
                 }
             }
         }
+
+        // Return an array of unique keywords
         return Array.from(keywords);
 
     }
@@ -170,11 +186,9 @@ async function main() {
 
     let sources = ['https://bbc.com', 'https://cincinnati.com', 'https://foxnews.com', 'https://npr.org/sections/news/', 'https://nytimes.com', 'https://forbes.com', 'https://wsj.com', 'https://www.cnn.com/', 'https://www.nbcnews.com/', 'https://abcnews.go.com/', 'https://www.yahoo.com/news/', 'https://www.washingtonpost.com/', 'https://www.theguardian.com/us', 'https://www.latimes.com/', 'https://www.apnews.com/', 'https://www.economist.com/', 'https://www.ap.org/en-us/', 'https://www.reuters.com/', 'https://www.bloomberg.com/', 'https://www.foreignaffairs.com/', 'https://www.theatlantic.com/', 'https://www.politico.com/', 'https://time.com/', 'https://www.cbsnews.com/'];
 
-    let outstandingReqs = 0;
     let receiveMessages = [];
     let explored = new Set();
     let uniqueKeywords = new Set();
-    let stopTime = -1;
     let total = 0;
     let flag = false;
     let batchsize = 5;
@@ -335,80 +349,101 @@ async function main() {
     } else {
         console.log('in worker node');
 
+        // Initialize some variables
         let local_unique = new Set();
-
         let time = [0];
         let global_links = [0];
         let global_kws = [0];
         let local_links = [0];
-        let [global_keywords, global_explored] = [0, 0];
+        let [global_keywords, global_explored] = [-1, -1];
 
         let it = 0;
-        while (stopTime < 0 || Date() < stopTime) {
+
+        while (true) {
 
             it++;
+
+            // Store previous counts so we can check if data has changed
             let [prev_global_keywords, prev_global_explored] = [global_keywords, global_explored];
 
+            // Receive from rank 0
             [global_keywords, global_explored, source_arr] = await mpi.irecv(0, 'default');
 
             console.log('received from 0: ', global_keywords, global_explored);
 
+            // Check how long it's been since we graphed last
             let last_time = time[time.length - 1];
             let this_time = timeSince(starttime);
 
-            // If the data has changed or it's been more than 10 seconds, update the graph
+            // If the data has changed or it's been more than 10 seconds since the last graph update, update the graph
             let change = prev_global_keywords != global_keywords || prev_global_explored != global_explored || this_time - last_time > 10;
             if (change) {
+
+                // Update the arrays to be graphed
                 time.push(this_time);
                 global_links.push(global_explored);
                 global_kws.push(global_keywords);
                 local_links.push(s.num_discovered_links);
-            }
-            let graph = {
-                type: 'line', data:
-                    {
-                        labels: time,
-                        datasets:
-                            [{label: 'Global Unique Links', data: global_links}, {
-                                label: 'Links discovered by this node',
-                                data: local_links
-                            }, {label: 'Global Unique Keywords', data: global_kws}]
+
+                // Build the graph object
+                let graph = {
+                    type: 'line', data:
+                        {
+                            labels: time,
+                            datasets:
+                                [{label: 'Global Unique Links', data: global_links}, {
+                                    label: 'Links discovered by this node',
+                                    data: local_links
+                                }, {label: 'Global Unique Keywords', data: global_kws}]
+                        }
+                };
+
+                // Update the UI and graph by sending graph object to quickchart.io
+                mpi.updateStatus({
+                    progress: {reset: true, increment: Math.floor(global_keywords / 2000 * 100)},
+                    'Global number of unique links discovered': global_explored,
+                    'Global number of unique keywords discovered': global_keywords,
+                    image: {
+                        type: 'img',
+                        src: 'https://quickchart.io/chart?width=1000&height=600&c=' + JSON.stringify(graph),
+                        width: 500,
+                        height: 300
                     }
-            };
+                });
+            }
 
-
-            mpi.updateStatus({
-                progress: {reset: true, increment: Math.floor(global_keywords / 2000 * 100)},
-                'Global number of unique links discovered': global_explored,
-                'Global number of unique keywords discovered': global_keywords,
-                image: {
-                    type: 'img',
-                    src: 'https://quickchart.io/chart?width=1000&height=600&c=' + JSON.stringify(graph),
-                    width: 500,
-                    height: 300
-                }
-            });
-
-
+            // Start processing received links
             let links = [];
             let keywords = [];
             let len = 0;
             console.log(source_arr);
             if (source_arr == ['']) {
                 await sleep(1);
-            } else {
+            }
+            else {
+
+                // Child receives an array of size <batchsize>, each element of which is a link to explore
                 for (let idx = 0; idx < source_arr.length; idx++) {
+
+                    // Trying to avoid rate limiting here
                     await sleep(1);
+
+                    // Look at single link
                     let source = source_arr[idx];
+                    console.log(`received link from root ${source}`);
+
                     let tmp = [];
-                    console.log('received link from root ' + source);
+
+                    // Trim the whitespace and scrape the URL
                     source = source.trim();
-                    //let parts = source.split('/');
-                    //let baseurl = parts.join('/');
                     s.setUrl(source);
                     let retval = await s.scrape();
-                    console.log('result of scrape: ' + retval)
+                    console.log(`result of scrape: ${retval}`);
+
+                    // Add the newly discovered keywords to the keywords array
                     keywords = keywords.concat(retval[0]);
+
+                    // Add the unique links to the links array
                     let all_links = retval[1];
                     if (all_links) {
                         for (let jdx = 0; jdx < all_links.length; jdx++) {
@@ -418,13 +453,14 @@ async function main() {
                                 local_unique.add(curr_link);
                                 len++;
                             }
-                            //console.log('links discovered: ' + links);
                         }
                     }
                     links.push(tmp)
                 }
-                console.log('sending discovered links to root ' + len.toString());
-                console.log('sending data to root ' + keywords);
+                console.log(`sending ${len.toString()} discovered links to root `);
+                console.log(`sending data to root ${keywords}`);
+
+                // Send the discovered data back to root and nap for a second
                 mpi.isend([keywords, links], 0, 'default');
                 await sleep(1);
             }
